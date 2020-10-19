691eddda0cac801f5ac0a420acc338ddb157f4f1
Emilia Bothe <emibo1999@gmail.com>
2020-10-19 16:57:55 +0200

    Version 1.2.2

--- uebung01_brodmann_bothe/src/uebung01_brodmann_bothe/Main.java
+++ uebung01_brodmann_bothe/src/uebung01_brodmann_bothe/Main.java
@@ -1,5 +1,14 @@
 package uebung01_brodmann_bothe;
 
+/**
+ * @authors: Emilia Maria Bothe (m26329), Paul Brodmann (m26405)
+ * @date 19.10.2020
+ * @version: 1.2.2
+ * Softwaretechnik: Uebung 01
+ *
+ * Description:
+ * Main Class.
+ */
 public class Main {
 
     public static void main(String[] args) {
--- uebung01_brodmann_bothe/src/uebung01_brodmann_bothe/MainMenuBar.java
+++ uebung01_brodmann_bothe/src/uebung01_brodmann_bothe/MainMenuBar.java
@@ -2,16 +2,19 @@ package uebung01_brodmann_bothe;
 
 import java.awt.*;
 
+/**
+ * deprectaed
+ */
 public class MainMenuBar extends MenuBar {
 
-    private Menu _menu;
+     /*private Menu _menu;
    
 
     public MainMenuBar(){
-        _menu = new Menu("Hintergrundfarbe-Programm");
-        /*_menu.add(new MenuItem("Farbe: rot")).addActionListener(this);
+       _menu = new Menu("Hintergrundfarbe-Programm");
+        _menu.add(new MenuItem("Farbe: rot")).addActionListener(this);
         _menu.add(new MenuItem("Farbe: gelb"));
-        _menu.add(new MenuItem("Farbe: grün"));*/
+        _menu.add(new MenuItem("Farbe: grün"));
 
         MenuItem rot = new MenuItem("Farbe: rot");
         MenuItem gelb = new MenuItem("Farbe: gelb");
@@ -23,6 +26,6 @@ public class MainMenuBar extends MenuBar {
 
 
         add(_menu);
-    }
+    }*/
 
 }
